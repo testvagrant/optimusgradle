b4b4854193d4d11937ed1f36d8d37a81c407583f
krishnb <kbandame@gmail.com>
2018-01-03 11:36:44 +0530

    changed optimus teardown to services

--- src/main/groovy/com/testvagrant/optimus/utils/OptimusTearDown.groovy
+++ src/main/groovy/com/testvagrant/optimus/utils/OptimusTearDown.groovy
@@ -1,14 +1,14 @@
 package com.testvagrant.optimus.utils
 
-import com.testvagrant.monitor.radiator.MongoWriter
+import com.testvagrant.monitor.services.BuildsServiceImpl
 import com.testvagrant.optimus.ReportMain
 
 class OptimusTearDown {
     static void updateBuildRecord() {
         println "updating build end time"
-        new MongoWriter().notifyBuildEnd();
+        new BuildsServiceImpl().notifyBuildEnd();
         println "updating build unique scenario count"
-        new MongoWriter().updateBuildWithUniqueScenarios();
+        new BuildsServiceImpl().updateBuildWithUniqueScenarios();
     }
 
     def static teardown() {
